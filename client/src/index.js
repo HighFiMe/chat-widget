7f6eef2ef4fdb880919055a19da5ce8aa343a24a
lazycoder1 <gautamgsabhahit@gmail.com>
2022-06-11 00:57:14 +0530

    adding intro text

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,11 @@ import Iframe from './components/iFrame';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div className="intro-text">
+      Highfi Me Test Client 
+      <br/>
+      Please click on the chat plugin bottom right to know more
+    </div>
     <Iframe accessToken='some-token'/>    
   </React.StrictMode>
 );
